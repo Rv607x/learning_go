40fe9a8c20f2d1d2535bfa79402168e02de31965
Rv607 <19752692+Rv607x@users.noreply.github.com>
2024-04-01 12:48:22 +0300

    Added Booleans type

---
+++ ch2/booleans.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// variables can have 2 values: true or false
+	var flag bool        // No value assigned, sets to false automatically
+	var isAwesome = true // assigned true value
+	fmt.Println(flag)
+	fmt.Println(isAwesome)
+}
