7d0b1277d314c71a6dbaf7fd20fd781cac7f0282
Rv607 <19752692+Rv607x@users.noreply.github.com>
2024-04-01 12:49:22 +0300

    added more information about bool in comments

--- ch2/booleans.go
+++ ch2/booleans.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// variables can have 2 values: true or false
+	// bool variables can have 2 values: true or false
 	var flag bool        // No value assigned, sets to false automatically
 	var isAwesome = true // assigned true value
 	fmt.Println(flag)
